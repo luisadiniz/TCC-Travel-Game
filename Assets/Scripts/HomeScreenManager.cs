314b7044965605f05811d8920a3645144a0024ca
Luisa Diniz <ldiniz@gazeus.com>
2021-09-12 20:16:31 -0300

    crating event system for all the scenes

--- Assets/Scripts/HomeScreenManager.cs
+++ Assets/Scripts/HomeScreenManager.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HomeScreenManager : MonoBehaviour
 {
     
     public void OnMapClick()
     {
-
+        Debug.Log("On Map click!");
+        SceneManager.LoadScene("Map");
     }
 
     public void OnQuizClick()
     {
-
+        SceneManager.LoadScene("Quiz");
     }
 
     public void OnStatsClick()
