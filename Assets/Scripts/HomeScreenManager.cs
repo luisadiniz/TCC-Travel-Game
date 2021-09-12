eeb3d059da5f1f8093aa9482032944c475e59b87
Luisa Diniz <ldiniz@gazeus.com>
2021-09-12 19:54:18 -0300

    creating extra scenes and changing ui

---
+++ Assets/Scripts/HomeScreenManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeScreenManager : MonoBehaviour
+{
+    
+    public void OnMapClick()
+    {
+
+    }
+
+    public void OnQuizClick()
+    {
+
+    }
+
+    public void OnStatsClick()
+    {
+        
+    }
+}
