8a9e03702400e7423b9aa7c7be1ccc3c1efdf626
Luisa Diniz <ldiniz@gazeus.com>
2021-09-12 21:44:02 -0300

    creating questions scriptable object and script

---
+++ Assets/Scripts/QuizQuestionsScriptable.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Quiz", menuName = "ScriptableObjects/Quiz Questions", order = 1)]
+public class QuizQuestionsScriptable : ScriptableObject
+{
+    public List<Question> questions;
+}
+
+[Serializable]
+public struct Question
+{
+  public string Statement;
+  public string FirstOption;
+  public string SecondOption;
+  public string ThirdOption;
+  public int RightOption;
+}
+    
